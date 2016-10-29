b29a63ae45e7d8e1b0f4801eaf8a67e16d2a4b4a
Yuto Tokunaga <yuntan.sub1@gmail.com>
2016-10-29 18:31:55 +0900

    :art: Clean up code

--- index.ts
+++ index.ts
@@ -1,32 +1,34 @@
 // pointer info (offset, pressure)
-interface Point {
+interface InkRenderingSegment {
   x: number,
   y: number,
   pressure: number,
-  mode: 'pen' | 'eraser',
+  mode: 'ink' | 'eraser',
 }
 
 const π = Math.PI;
 
-const canvas = document.querySelector('canvas.draw-layer') as HTMLCanvasElement;
+const inkLayer = document.querySelector('canvas.ink-layer') as HTMLCanvasElement;
 // drawing eraser marker with DOM at nearly 60times/sec is too heavy task, so
 // use canvas instead
 // const eraserMarker = document.querySelector('.canvas-eraser-marker') as HTMLDivElement;
 const markerLayer = document.querySelector('canvas.marker-layer') as HTMLCanvasElement;
 
 window.addEventListener('load', _ev => {
+  // adjust canvas size to fit window
   const w = window.innerWidth, h = window.innerHeight;
-  canvas.width = w; canvas.height = h;
+  inkLayer.width = w; inkLayer.height = h;
   markerLayer.width = w; markerLayer.height = h;
 });
 
+const eraserButton = 32; // 6th button
 const penPressureSensitivity = 5;
 const eraserPressureSensitivity = 100;
 const penStyle = '#001128'; // LAMY blue black ink
 const eraserStyle = '#fff';
 
 // pointer info buffer
-let tracks = [] as Array<Point>;
+let inkStroke = [] as Array<InkRenderingSegment>;
 // for requestAnimationFrame
 let requestID: number;
 
@@ -34,105 +36,111 @@ let requestID: number;
 let pointerCount = 0;
 let drawCount = 0;
 
-canvas.addEventListener('pointerdown', ev => {
+const isEraser = (ev: PointerEvent) => (ev.buttons | eraserButton) === ev.buttons;
+
+inkLayer.addEventListener('pointerdown', ev => {
+  if (ev.pointerType !== 'pen' || ev.pressure === 0) { return; }
   ev.preventDefault();
-  tracks = []; // clear buffer
+
+  const rect = (ev.target as HTMLCanvasElement).getBoundingClientRect();
+  console.assert(ev.offsetX === ev.clientX - rect.left, "The value of ev.offsetX is invalid");
+
+  inkStroke = []; // clear buffer
+  inkStroke.push({
+    x: ev.offsetX,
+    y: ev.offsetY,
+    pressure: ev.pressure,
+    mode: isEraser(ev) ? 'eraser' : 'ink',
+  });
+
   requestID = requestAnimationFrame(draw); // request for draw loop
-  console.log('pointerdown');
 });
 
-canvas.addEventListener('pointermove', ev => {
+inkLayer.addEventListener('pointermove', ev => {
+  if (ev.pointerType !== 'pen' || ev.pressure === 0) { return; }
   ev.preventDefault();
 
-  // if (ev.pointerType !== 'pen' || ev.pressure === 0) { return; }
-  if (ev.pressure === 0) { return; }
-
-  // const rect = (ev.target as HTMLCanvasElement).getBoundingClientRect();
-  // console.assert(ev.offsetX === ev.clientX - rect.left, "The value of ev.offsetX is invalid");
-
-  tracks.push({
+  inkStroke.push({
     x: ev.offsetX,
     // x: ev.clientX - rect.left,
     y: ev.offsetY,
     // y: ev.clientY - rect.top,
     pressure: ev.pressure,
     // if the 6th button is pressed, switch to eraser
-    mode: (ev.buttons | 32) === ev.buttons ? 'eraser' : 'pen',
+    mode: isEraser(ev) ? 'eraser' : 'ink',
   });
 });
 
-canvas.addEventListener('pointerup', ev => {
+inkLayer.addEventListener('pointerup', ev => {
+  if (ev.pointerType !== 'pen') { return; }
   ev.preventDefault();
-  tracks = [];
+
+  inkStroke = []; // clear buffer
   cancelAnimationFrame(requestID); // break draw loop (for less CPU consumption)
-  console.log('pointerup');
-  console.log(`average pointer count: ${pointerCount / drawCount}`);
+  console.log(`[pointerup] average pointer count: ${pointerCount / drawCount}`);
   pointerCount = 0; drawCount = 0;
+});
+
+inkLayer.addEventListener('pointerleave', ev => {
+  if (ev.pointerType !== 'pen') { return; }
+  ev.preventDefault();
+
+  inkStroke = []; // clear buffer
+  cancelAnimationFrame(requestID); // break draw loop (for less CPU consumption)
 });
 
 // canvas draw loop
 function draw() {
-  const ctx = canvas.getContext('2d') !;
-
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   // if buffer count equals 0 or 1, do nothing
-  if (tracks.length > 1) {
-    const {x, y, pressure, mode} = tracks[0];
+  if (inkStroke.length > 1) {
+    const inkCtx = inkLayer.getContext('2d') !;
+    const markerCtx = markerLayer.getContext('2d') !;
+    const {x, y, pressure, mode} = inkStroke[0];
 
     switch (mode) {
-      case 'pen':
-        ctx.strokeStyle = penStyle;
-        ctx.lineWidth = pressure * penPressureSensitivity;
-        // eraserMarker.style.display = 'none';
+      case 'ink':
+        inkCtx.strokeStyle = penStyle;
+        inkCtx.lineWidth = pressure * penPressureSensitivity;
+        inkCtx.lineCap = 'round';
+
+        inkCtx.beginPath();
+        inkCtx.moveTo(x, y);
+        for (const p of inkStroke.slice(1)) {
+          inkCtx.lineTo(p.x, p.y);
+        }
+        inkCtx.stroke();
+
+        // clear eraser marker
+        markerCtx.clearRect(0, 0, markerCtx.canvas.width, markerCtx.canvas.height);
         break;
 
       case 'eraser':
         let width = pressure * eraserPressureSensitivity;
-        ctx.strokeStyle = eraserStyle;
-        ctx.fillStyle = eraserStyle;
-        ctx.lineWidth = width;
+        inkCtx.fillStyle = eraserStyle;
 
-        // eraserMarker.style.display = 'block';
-        // eraserMarker.style.width = width + 'px';
-        // eraserMarker.style.height = width + 'px';
-        // eraserMarker.style.left = (x - width / 2) + 'px';
-        // eraserMarker.style.top = (y - width / 2) + 'px';
-        const ctx_ = markerLayer.getContext('2d') !;
-        ctx_.strokeStyle = '#888';
-        ctx_.setLineDash([4, 4]);
-        ctx_.lineWidth = 2;
-        ctx_.clearRect(0, 0, ctx_.canvas.width, ctx_.canvas.height);
-        ctx_.beginPath();
-        ctx_.ellipse(x, y, width / 2, width / 2, 0, 0, 2 * π);
-        ctx_.stroke();
+        inkCtx.beginPath();
+        for (const p of inkStroke) {
+          const radius = p.pressure * eraserPressureSensitivity / 2;
+          inkCtx.ellipse(x, y, radius, radius, 0, 0, 2 * π);
+          inkCtx.fill();
+        }
+
+        // draw eraser marker
+        markerCtx.strokeStyle = '#888';
+        markerCtx.setLineDash([4, 4]);
+        markerCtx.lineWidth = 2;
+
+        markerCtx.clearRect(0, 0, markerCtx.canvas.width, markerCtx.canvas.height);
+        markerCtx.beginPath();
+        markerCtx.ellipse(x, y, width / 2, width / 2, 0, 0, 2 * π);
+        markerCtx.stroke();
         break;
     }
-    // ctx.lineWidth = 2;
-    ctx.lineCap = 'round';
 
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    for (const p of tracks.slice(1)) {
-      switch (mode) {
-        case 'pen':
-          ctx.lineTo(p.x, p.y);
-          break;
-
-        case 'eraser':
-          const radius = p.pressure * eraserPressureSensitivity / 2;
-          ctx.ellipse(x, y, radius, radius, 0, 0, 2 * π);
-          ctx.fill();
-          break;
-      }
-    }
-    // ctx.closePath();
-    if (mode === 'pen') { ctx.stroke(); }
-
-    pointerCount += tracks.length - 1;
+    pointerCount += inkStroke.length - 1;
     drawCount += 1;
 
-    tracks = [tracks[tracks.length - 1]];
+    inkStroke = [inkStroke[inkStroke.length - 1]]; // create new buffer with last element
   }
 
   requestID = requestAnimationFrame(draw); // continue draw loop
